a324b2012e498a185b64d46477454c492e034111
timothiasthegreat <tim@firesideit.ca>
2024-10-29 14:21:23 -0600

    Remove obsolete README files and Huntress functions; add new configuration file for Halo and Huntress integration

---
+++ Scripts/Menu/Map Huntress Companies.ps1
@@ -0,0 +1,94 @@
+Write-Host "Beginning to map Huntress Clients to Halo Companies" -ForegroundColor Cyan
+Write-Log -message "Beginning to map Huntress Clients to Halo Companies"
+$mappingoutput = Join-Path -Path $basepath -ChildPath "MappingOutput_$($date).csv"
+$supplierID = $config.huntress.halosupplierID
+Write-Host "Getting Current Mapping" -ForegroundColor Cyan
+Write-Log -message "Getting Current Mapping"
+Try {
+    $CurrentMapping = Get-HaloPublishedReport $config.halo.syncmapreport
+} catch {
+    Write-Host "Failed to get current mapping.  Please check the report and try again." -ForegroundColor Red
+    Write-Log -message "Failed to get current mapping.  Please check the report and try again. Exiting to Menu."
+    Show-Menu
+}
+Write-Host "Getting Huntress Organizations" -ForegroundColor Cyan
+write-log -message "Getting Huntress Organizations"
+try {
+    $huntressOrgs = Get-HuntressOrgs
+} catch {
+    Write-Host "Failed to get Huntress Organizations.  Please check the API and try again."
+    Write-Log -message "Failed to get Huntress Organizations.  Please check the API and try again. Exiting to Menu."
+    Show-Menu
+}
+$mapping = @()
+$failedmapping = @()
+foreach ($huntressOrg in $huntressOrgs.organizations) {
+    $mappingID = $CurrentMapping | Where-Object { $_.CFbillingsyncID -eq $huntressOrg.id }
+    if ($null -eq $mappingID) {
+        try {
+            $haloOrg = Get-HaloOrg -companyname $huntressOrg.name
+        } catch {
+            Write-Host "Failed to get Halo Company for $($huntressOrg.name).  Please check the API and try again." -ForegroundColor Red
+            Write-Log -message "Failed to get Halo Company for $($huntressOrg.name).  Please check the API and try again."
+        }
+        if ($haloOrg.record_count -eq 0) { 
+            Write-Host "Halo Company not found for $($huntressOrg.name). You will need to map manually." -ForegroundColor Yellow
+            Write-Log -message "Halo Company not found for $($huntressOrg.name). You will need to map manually."
+            $failedmapping += [PSCustomObject]@{
+                huntressID = $huntressOrg.id
+                huntressName = $huntressOrg.name
+            } 
+        } elseif ($haloOrg.record_count -eq 1) {
+            Write-Log -message "Mapping $($huntressOrg.name) to $($haloOrg.clients.name)"
+            $mapping += [PSCustomObject]@{
+                supplierID = $supplierID
+                supplierclientID = $huntressOrg.id
+                supplierclientName = $huntressOrg.name
+                HalocustomerID = $haloOrg.clients.id
+                HalocustomerName = $haloOrg.clients.name
+            }
+        } else {
+            Write-Host "Multiple Halo Companies found for $($huntressOrg.name). You will need to map manually." -ForegroundColor Yellow
+            $failedmapping += [PSCustomObject]@{
+                huntressID = $huntressOrg.id
+                huntressName = $huntressOrg.name
+            }
+        }
+    } Else { 
+        Write-Host "Mapping already exists for $($huntressOrg.name) to $($mappingID.CFbillingsyncID)" -ForegroundColor Cyan
+    }
+}
+function Get-MappingDecision {
+    Write-Host "There are $($mapping.count) mappings to add." -ForegroundColor Green
+    Write-Host "Enter A to add mappings to the table, V to view mappings, F to view failed mappings, or any other key to exit" -ForegroundColor Green
+    $addtomap = Read-Host
+    if ($addtomap -eq "A") {
+        try {
+            $tableupdate = Update-Table -data $mapping
+            Write-Host "Table updated with $($tableupdate.count) records"
+            Write-Log -message "Table updated with $($tableupdate.count) records"
+            $tableupdatelog = $tableupdate | Select-Object -Property SupplierCompany,status,response
+            $tableupdatelog >> $mappingoutput
+            Write-Host "Table update log saved to $mappingoutput" -ForegroundColor Green
+            write-log -message "Table update log saved to $mappingoutput"
+        } catch {
+            Write-Host "Failed to update table.  $($_.Exception.Message)" -ForegroundColor Red
+            Write-Log -message "Failed to update table.  $($_.Exception.Message)"
+        }
+    } elseif ($addtomap -eq "V") {
+        $mapping | Out-GridView -Wait
+        $addtomap = $null
+        Get-MappingDecision
+    }
+    elseif ($addtomap -eq "F") {
+        $failedmapping | Out-GridView -Wait
+        $addtomap = $null
+        Get-MappingDecision
+    }
+    else {
+        Write-Host "User declined to add mappings to table"
+    }
+}
+Get-MappingDecision
+
+Write-log -message "Mapping Huntress Clients to Halo Companies complete. Returning to Menu."
--- menu.ps1
+++ menu.ps1
@@ -1,31 +1,21 @@
-#placeholder for the launch menu
-$scripts = Get-ChildItem -Path $PSScriptRoot\scripts\ -Filter *.ps1
-$config = Get-Content -Path $PSScriptRoot\Config\variables.json | ConvertFrom-Json
+#dot source include function files
+Get-ChildItem -Path $PSScriptRoot\Functions\ -Filter 'func_*.ps1' -Recurse | ForEach-Object { . $_.FullName }
+#Load Scripts and Config
+$scripts = Get-ChildItem -Path $PSScriptRoot\scripts\Menu -Filter *.ps1
+$config = Get-Content -Path $PSScriptRoot\Config\config.json | ConvertFrom-Json
+$basepath = $PSScriptRoot
 $date = Get-Date -Format "yyyy-MM-dd"
-$logfile = Join-Path -Path $config.logpath -ChildPath "$date.log"
-Start-Transcript -Path $logfile -Append
-function Show-Menu {
-    Write-Host "Welcome to the Launch Menu"
-    Write-Host "Please select an option from the list below"
-    $i = 1
-    foreach ($script in $scripts) {
-        Write-Host "$i. $($script.BaseName)"
-        $i++
-    }
-    Write-Host "$i. Exit"
-    $selection = Read-Host "Please select an option"
-    if ($selection -eq $i) {
-        Write-Host "Exiting"
-        Stop-Transcript
-        Exit
-    } elseif ($selection -gt $scripts.Count) {
-        Write-Host "Invalid selection"
-        Show-Menu
-    } else {
-        $script = $scripts[$selection - 1]
-        . $script.FullName
-        Show-Menu
-    }
+$logfile = Join-Path -Path $PSScriptRoot -ChildPath "$date.log"
+Write-Log -message "###############################################"
+Write-Log -message "Billing Sync Script Started"
+Write-Log -message "###############################################"
+#Before showing the menu we want to make sure the keyvault is configured and unlocked
+Write-log -message "Verifying Keyvault is configured"
+try {
+    Test-Keyvault
+} catch {
+    Write-Host "Failed to verify Keyvault.  Please check the configuration and try again." -ForegroundColor Red
+    Write-Host "Keyvault must be configured and unlocked to run this script." -ForegroundColor Red
+    Exit
 }
-
 Show-Menu
